f02e218377ff93c472582e072a2e0656f7a4ec34
Andrew Hoefling <andrew@hoefling.me>
2019-05-13 16:56:24 -0400

    Added dnn dependency injection library and created startup file to register services.

---
+++ src/Dnn.DependencyInjection.Samples.Razor3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Dnn.DependencyInjection.Samples.Razor3.Models;
+using DotNetNuke.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dnn.DependencyInjection.Samples.Razor3
+{
+    public class Startup : IDnnStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IndexModel>();
+        }
+    }
+}
